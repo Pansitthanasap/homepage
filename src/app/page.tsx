902bd534dcc1a473ca1e7c852a52be2575e8dd2b
Vlachodimos <sales@thewantech.com>
2025-10-26 20:47:04 +0700

    Refactor code structure for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,44 @@
 import Image from "next/image";
 import handshake from "../../public/handshake.png";
+import contact_us_bg from "../../public/contact-us-bg.png";
+import "./globals.css";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
+      {/* Homepage */}
       <div className="relative flex flex-col h-screen bg-[#073328]">
-          <Image
-            src={handshake}
-            alt="Handshake"
-            className="w-full"
-            draggable={false}
-          />
-          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full gap-6 flex flex-col">
-            <div className="font-[Crimson_Text] text-[80px] text-white text-center">
-              We Build Teams That Build Your Business
-            </div>
-            <div className="text-[32px] leading-[85%] text-white text-center">
-              เราสร้างทีมให้คุณ เพื่อสร้างธุรกิจให้โต
+        <Image
+          src={handshake}
+          alt="Handshake"
+          className="w-full"
+          draggable={false}
+        />
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full gap-6 flex flex-col">
+          <div className="font-[Crimson_Text] text-[80px] text-white text-center">
+            We Build Teams That Build Your Business
+          </div>
+          <div className="text-[32px] leading-[85%] text-white text-center">
+            เราสร้างทีมให้คุณ เพื่อสร้างธุรกิจให้โต
+          </div>
+        </div>
+      </div>
+      {/* Contact Us*/}
+      <div className="relative flex flex-col">
+        {/* Background image */}
+        <div className="flex justify-center items-center">
+          <div className="absolute top-[45px] bg-white text-black px-4 py-2 rounded-[50px] w-[1024px] h-auto ">
+            <div className="font-bold text-[40px] leading-[140%] tracking-[0%] text-center">
+              ติดต่อเรา
             </div>
           </div>
+        </div>
+        <Image
+          src={contact_us_bg}
+          alt="contact us bg"
+          className="w-auto h-screen object-cover"
+          draggable={false}
+        />
       </div>
     </div>
   );
